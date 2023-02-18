e128e27ed7cdd769baacbed6d3e8a8162489013f
Jord525 <dorohenkojord@gmail.com>
2023-02-18 20:18:30 +0300

    security for app

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from "express";
 import mongoose from "mongoose"
 import cors from 'cors'
+import helmet from "helmet";
 
 import { registerValidation } from './validations/auth.js'
 import { register } from './controllers/UserController.js'
 import { tablesValidation } from "./validations/tables.js";
 import { create, findAll, findOne, remove, update } from "./controllers/TableController.js";
 
+
 const app = express()
 app.use(cors())
+app.use(helmet)
 app.use(express.json())
 
+const port = process.env.PORT
 
 mongoose.set('strictQuery', false);
 mongoose.connect(
-    'mongodb+srv://Jord:12345@cluster0.r8hh2et.mongodb.net/blogd?retryWrites=true&w=majority'
+    `mongodb+srv://${process.env.AUTHUSER}:${process.env.PASSWORD}@cluster0.r8hh2et.mongodb.net/blogd?retryWrites=true&w=majority`
 ).then(() => {
     console.log('db ok')
 }).catch((err) => {
@@ -35,6 +41,6 @@ app.post('/auth/register', registerValidation, register)
 
 
 
-app.listen(3333, () => {
-    console.log('server started')
+app.listen(port, () => {
+    console.log(`server started on port ${port}`)
 })
